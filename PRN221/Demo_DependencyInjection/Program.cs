3afa6ba63f5e51b875375b05f36c30346cbfd372
sowndv02 <daoson03112002@gmail.com>
2024-01-16 22:44:58 +0700

    Update 16/01/2023 22h44p

---
+++ PRN221/Demo_DependencyInjection/Program.cs
@@ -0,0 +1,38 @@
+﻿
+class DongCoOtoTQ : IDongCo
+{
+    public void NoMay()
+    {
+        Console.WriteLine("Dong co dang chay");
+    }
+}
+
+class DongCoOtoMy : IDongCo
+{
+    public void NoMay()
+    {
+        Console.WriteLine("Dong co dang chay");
+    }
+}
+
+
+interface IDongCo
+{
+    public void NoMay();
+}
+class Oto
+{
+    public void DiChuyen(IDongCo dongco)
+    {
+        dongco.NoMay();
+        Console.WriteLine("Xe di chuyen");
+    }
+
+    public static void Main(string[] args)
+    {
+        IDongCo dc = new DongCoOtoTQ();
+        Oto oto = new Oto();
+        oto.DiChuyen(dc);
+        Console.ReadLine();
+    }
+}
---
+++ PRN221/Demo_RazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo_RazorPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public string Name {  get; set; }   
+        public void OnGet()
+        {
+            /*
+             MVC: ViewBag, ViewData, Model, Tempdata
+             Razor kh co viewbag
+             */
+            Name = "Tran Van B";
+        }
+    }
+}
